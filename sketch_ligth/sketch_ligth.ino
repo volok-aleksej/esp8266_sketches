60d433314f9db6893a75eabeb696027e985c5c0a
Alexey Volokitin <alexey.v@didww.com>
2019-02-07 14:22:57 +0200

    restruct repository, fix autocomplete for sketch ligth

--- sketch_ligth/sketch_ligth.ino
+++ sketch_ligth/sketch_ligth.ino
@@ -5,6 +5,8 @@ char motion[] = "motion";
 char noMotion[] = "no motion";
 char ligthOff[] = "Ligth OFF";
 char ligthOn[] = "Ligth ON";
+char on[] = "on";
+char off[] = "off";
 
 bool status = false;
 bool sensorValue = false;
@@ -12,14 +14,39 @@ int analogValue;
 bool releStatus = false;
 unsigned long ligthTimeStart = 0;
 
-void onligth(const twnstd::vector<String>& params)
+struct Light
 {
-  if(const_cast<twnstd::vector<String>&>(params)[0] == "on") {
-    digitalWrite(12, HIGH);
-  } else if(const_cast<twnstd::vector<String>&>(params)[0] == "off") {
-    digitalWrite(12, LOW);
-  }
-}
+    void doCommand(const twnstd::vector<String>& params)
+    {
+        if(!params.length()) {
+            LOG_ERROR("absent value of  ligth command, use '%s' or '%s'", on, off);
+        } else if(const_cast<twnstd::vector<String>&>(params)[0] == "on") {
+            digitalWrite(12, HIGH);
+            releStatus = true;
+        } else if(const_cast<twnstd::vector<String>&>(params)[0] == "off") {
+            digitalWrite(12, LOW);
+            releStatus = false;
+        } else {
+            LOG_ERROR("unknown value of  ligth command, use '%s' or '%s'", on, off);
+        }
+    }
+
+    twnstd::vector<String> getNextCommandArgs(const twnstd::vector<String>& params)
+    {
+        if(params.length() == 1 && (const_cast<twnstd::vector<String>&>(params)[0] == on || const_cast<twnstd::vector<String>&>(params)[0] == off)) {
+            return twnstd::vector<String>();
+        }
+        if(params.length() <= 1) {
+            twnstd::vector<String> args;
+            args.push_back(on);
+            args.push_back(off);
+            return args;
+        }
+        return twnstd::vector<String>();
+    }
+};
+
+Light onligth;
 
 void printStatus(const twnstd::vector<String>& params)
 {
